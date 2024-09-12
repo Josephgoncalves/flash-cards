1e685e3c641bce1b5146ce0d07e169e67c67cce9
Josephgoncalves <134930337+Josephgoncalves@users.noreply.github.com>
2024-09-12 12:12:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Geografia',
+    'Qual é o menor pais do mundo em área?',
+    'Dentro de Roma, capital da Itália, está localizado o menor país do mundo: o Vaticano'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o líder militar que conquistou grande parte da Europa durante o século XIX?',
+    'Napoleão Bonaparte.'
+)
+
+criaCartao(
+    'Português',
+    ' Qual é a diferença entre "por que" e "porque?',
+    '"Por que" é usado em perguntas, "porque" é usado para responder ou explicar.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um triângulo?',
+    'A = (base × altura) / 2.'
+)
+
+criaCartao(
+    'Sociologia',
+    ' Quem é considerado o pai da sociologia?',
+    'Auguste Comte'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química do cloreto de sódio??',
+    'NaCl'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a unidade básica da vida?',
+    'A célula'
+)
+criaCartao(
+    'Física',
+    'Qual é a função do DNA?',
+    'Armazenar e transmitir informações genéticas.'
+)
+criaCartao(
+    'Física',
+    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
+    'Efeito fotoelétrico'
+)
+
